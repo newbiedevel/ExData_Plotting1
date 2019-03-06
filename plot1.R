91b37e619aefd88e63d45996a47217c9118f1450
newbiedevel <45289635+newbiedevel@users.noreply.github.com>
2019-03-06 23:06:07 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,15 @@
+library("data.table")
+setwd("./DATA SCIENCE COURSE/Exploratory_Data_Analysis/project2")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = paste(path,"dataFiles.zip", sep = "/"))
+unzip(zipfile = "data.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+# Prevents histogram from printing in scientific notation
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+barplot(totalNEI[, Emissions], names = totalNEI[, year], xlab = "Years", ylab = "Emissions", main = "Emissions over the Years")
